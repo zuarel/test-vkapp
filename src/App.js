8bf7b7924badf6e258d6d7e707fd5bbeffd5e230
zuarel <bubl174rus@gmail.com>
2019-10-02 20:01:40 +0500

    test

--- src/App.js
+++ src/App.js
@@ -6,38 +6,42 @@ import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
 import Persik from './panels/Persik';
+// import Test from './panels/Test';
+
+console.log(window.location);
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+    const [activePanel, setActivePanel] = useState('home');
+    const [fetchedUser, setUser] = useState(null);
+    const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+    const [t, setTest] = useState("");
 
-	useEffect(() => {
-		connect.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await connect.sendPromise('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+    useEffect(() => {
+        connect.subscribe(({ detail: { type, data } }) => {
+            if (type === 'VKWebAppUpdateConfig') {
+                const schemeAttribute = document.createAttribute('scheme');
+                schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+                document.body.attributes.setNamedItem(schemeAttribute);
+            }
+        });
+        async function fetchData() {
+            const user = await connect.sendPromise('VKWebAppGetUserInfo');
+            setUser(user);
+            setPopout(null);
+        }
+        fetchData();
+    }, []);
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+    const go = e => {
+        setActivePanel(e.currentTarget.dataset.to);
+    };
 
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
-		</View>
-	);
+    return (
+        <View activePanel={activePanel} popout={popout}>
+            <Home id='home' fetchedUser={fetchedUser} go={go} />
+            <Persik id='persik' go={go} />
+        </View>
+    );
 }
 
 export default App;
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -9,39 +9,35 @@ import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group title="User Data Fetched with VK Connect">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
-
-		<Group title="Navigation Example">
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
+    <Panel id={id}>
+        <PanelHeader>Тест панель хедер</PanelHeader>
+        <Group>
+            <Div>
+                Hello world 1
+            </Div>
+            <Div>
+                <Button size="xl" level="secondary">Подписаться</Button>
+            </Div>
+        </Group>
+        <Group>
+            <Div>
+                Hello world 2
+            </Div>
+        </Group>
+    </Panel>
 );
 
 Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
+    id: PropTypes.string.isRequired,
+    go: PropTypes.func.isRequired,
+    fetchedUser: PropTypes.shape({
+        photo_200: PropTypes.string,
+        first_name: PropTypes.string,
+        last_name: PropTypes.string,
+        city: PropTypes.shape({
+            title: PropTypes.string,
+        }),
+    }),
 };
 
 export default Home;
