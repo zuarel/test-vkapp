891ae18fa154d884d04049a432b1574d0212c4f9
zuarel <bubl174rus@gmail.com>
2019-10-02 17:53:57 +0500

    Updates

---
+++ precache-manifest.2e14f219f70ef278fc05c962e0efb620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759fb8f51e844214bb76d4e6442f3e54",
+    "url": "/test-vkapp/index.html"
+  },
+  {
+    "revision": "3c980771daefad709dba",
+    "url": "/test-vkapp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "f213db8f336d03ec2845",
+    "url": "/test-vkapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3c980771daefad709dba",
+    "url": "/test-vkapp/static/js/2.e95ce9ba.chunk.js"
+  },
+  {
+    "revision": "f213db8f336d03ec2845",
+    "url": "/test-vkapp/static/js/main.ccf5f50d.chunk.js"
+  },
+  {
+    "revision": "71605f7eae8d7f87b27e",
+    "url": "/test-vkapp/static/js/runtime-main.51a999cd.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test-vkapp/static/media/persik.4e1ec840.png"
+  }
+]);
